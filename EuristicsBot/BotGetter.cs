55c08b59b567afa9f7d9bb0b7238ee235d2a880a
ModernViolence <longlivethenewfl3sh@gmail.com>
2017-04-19 22:24:35 +0500

    added group answers

--- EuristicsBot/BotGetter.cs
+++ EuristicsBot/BotGetter.cs
@@ -1,8 +1,6 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InlineQueryResults;
-using Telegram.Bot.Types.InputMessageContents;
 
 namespace EuristicsBot
 {
@@ -16,8 +14,7 @@ namespace EuristicsBot
 			Bot.OnMessage += BotOnMessageReceived;
 			Bot.OnMessageEdited += BotOnMessageReceived;
 			Bot.OnReceiveError += BotOnReceiveError;
-			Bot.OnInlineQuery += BotOnInlineQueryReceived;
-
+			
 			return Bot;
 		}
 
@@ -33,7 +30,8 @@ namespace EuristicsBot
 			Logger.Log.Info($"Recieved: {message.Text}");
 			if (message == null || message.Type != MessageType.TextMessage) return;
 
-			if (message.Text.ToLower().Contains("как") && message.Text.ToLower().Contains("тест"))
+			if (message.Text.StartsWith("/howtotest") ||
+			    message.Text.ToLower().Contains("как") && message.Text.ToLower().Contains("тест"))
 			{
 				var nextEuristic = EuristicGetter.GetRandomEuristic();
 				await Bot.SendTextMessageAsync(message.Chat.Id, nextEuristic);
@@ -41,39 +39,9 @@ namespace EuristicsBot
 			else
 			{
 				await Bot.SendTextMessageAsync(message.Chat.Id,
-					"Если нет идей, как протестировать приложение — спроси меня, \"Как протестировать %имя релиза%?\". Пуллреквесты можно слать в https://github.com/24twelve/HowToTestBot.",
+					"Спроси меня: \"Как тестировать <подставь название>?\".\r\n\r\nПуллреквесты можно слать в https://github.com/24twelve/HowToTestBot.",
 					disableWebPagePreview: true);
-				//todo buttons
 			}
-		}
-
-		private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
-		{
-			var recievedMessage = inlineQueryEventArgs.InlineQuery.Query;
-			Logger.Log.Info($"Recieved: {recievedMessage}");
-			string answer;
-			if (recievedMessage.ToLower().Contains("как") && recievedMessage.ToLower().Contains("тест"))
-			{
-				answer = EuristicGetter.GetRandomEuristic();
-			}
-			else
-			{
-				answer = "Спроси меня, как протестировать что-то.";
-			}
-
-			InlineQueryResult[] results =
-			{
-				new InlineQueryResultNew
-				{
-					InputMessageContent = new InputTextMessageContent
-					{
-						MessageText = answer
-					}
-				}
-			};
-
-			await Bot.AnswerInlineQueryAsync(inlineQueryEventArgs.InlineQuery.Id, results, isPersonal: true,
-				cacheTime: 0);
 		}
 	}
 }
